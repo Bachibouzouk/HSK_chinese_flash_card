f50b8e9d6890475c52325732cfd4432aceb32463
PF <pfduc@bitbucket.org>
2016-02-02 10:43:13 -0500

    want to pull but they were apparently local changes

--- hsk_flashcard.py
+++ hsk_flashcard.py
@@ -3,7 +3,6 @@
 Created on Sat Jan 23 12:27:18 2016
 
 IMPROVEMENT :
-Give the possibility to the user to say if they like a new word (like a short term objective to learn)
 Then give the user possibilities to browse HSK * words only (without including all the lower HSK words)
 Or browse only the liked words
 
@@ -117,7 +116,10 @@ class HSKGui(QWidget):
         self.question_stage = ASK_WORD
         # these are the options availiable
         self.option_list = {"Save current session": self.save_learning,
-                            "Credits(disabled)": None, "Read the help(disabled)": None, "Reset the known voc": self.reset_voc_list, "Load another list": self.load_voc_list}
+                            "Credits(disabled)": None, 
+                            "Read the help(disabled)": None,
+                            "Reset the known voc": self.reset_voc_list, 
+                            "Load another list": self.load_voc_list}
         # this is the number of word one went through during one session
         self.numword = 0
 
@@ -256,8 +258,7 @@ class HSKGui(QWidget):
     def save_learning(self):
         """prompt the user to save the current session in a file"""
 
-        fname = str(QFileDialog.getSaveFileName(self, 'Save as', './%s' %
-                                                (os.path.basename(self.browser.fname)), "*.csv"))
+        fname = str(QFileDialog.getSaveFileName(self, 'Save as', './%s'%(os.path.basename(self.browser.fname)), "*.csv"))
         if fname:
             self.browser.save_voc_list(fname)
         else:
@@ -397,6 +398,6 @@ class FlashCardBrowser(object):
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
-    ex = HSKGui(fname="HSK_Level_5.csv")
+    ex = HSKGui(fname="HSK_Level_5_PF.csv")
     ex.show()
     sys.exit(app.exec_())
